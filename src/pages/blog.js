4b7d75036dba5cd9d80c5bcb2f6b2955a4f23d2f
Subho Karmakar <isubhokarma@gmail.com>
2019-07-29 20:29:17 +0530

    blog list styling added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, graphql, useStaticQuery } from "gatsby";
 
 import Layout from "../components/Layout";
+import blogStyles from "./blog.module.scss";
 
 // fetch the slug
 // use slug to generate a link to the post page
@@ -31,10 +32,10 @@ export default function blog() {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li>
+            <li className={blogStyles.post}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
