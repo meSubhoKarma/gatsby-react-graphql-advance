28af3b450ab7156b74c141bb6ead2287ad68990b
Subho Karmakar <isubhokarma@gmail.com>
2019-07-29 09:58:59 +0530

    render Blog (slug) pages from md to Blog Template

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,36 @@ module.exports.onCreateNode = ({ node, actions }) => {
     });
   }
 };
+
+// get path to template
+// get markdown data
+// create new pages
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const blogTemplate = path.resolve("./src/templates/Blog.js");
+
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug
+      }
+    });
+  });
+};
