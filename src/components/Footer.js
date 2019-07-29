e571a1ba54903f5b0eae907494c84ca6fe6428f3
Subho Karmakar <isubhokarma@gmail.com>
2019-07-29 20:20:00 +0530

    Footer styling added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 
+import footerStyles from "./footer.module.scss";
+
 export default function Footer() {
   const data = useStaticQuery(
     graphql`
@@ -15,7 +17,7 @@ export default function Footer() {
   );
 
   return (
-    <div>
+    <div className={footerStyles.footer}>
       <p>Created by {data.site.siteMetadata.author}, &copy; 2019</p>
     </div>
   );
