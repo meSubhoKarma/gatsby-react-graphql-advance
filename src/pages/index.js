7e068fe4c72af263a5de78d9b1eeee723d8995d7
Subho Karmakar <isubhokarma@gmail.com>
2019-07-30 12:05:05 +0530

    Helmet initial setup done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import Head from "../components/Head";
 
 export default function index() {
   return (
     <Layout>
+      <Head />
       <h1>Hello.</h1>
       <p>
         <Link to="/contact">Contact</Link>
       </p>
     </Layout>
-  )
+  );
 }
